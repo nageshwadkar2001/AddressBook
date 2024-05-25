0d10279de901dc5e3a710e76ffb66d72b21ba5f6
nageshwadkar2001 <nageshwadkar25@gmail.com>
2024-05-25 11:51:40 +0530

    branch UC1_AddressBook_Add

---
+++ src/AddressBook.java
@@ -0,0 +1,22 @@
+public class AddressBook {
+    String firstName;
+    String lastName;
+    String Address;
+    String city;
+    String state;
+    int zip;
+    String email;
+
+    public AddressBook(String firstName,String lastName,String Address,String city,String state,int zip,String email){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.Address=Address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.email=email;
+
+
+    }
+
+}
